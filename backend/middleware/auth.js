2dd5c018e213b4be4da7782626f02196b1efb55d
ewanouchka <ewanouchka@gmail.com>
2021-11-04 08:52:05 +0100

    suppr throw err

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,6 @@ module.exports = (req, res, next) => {
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       res.status(403).json({ message: "unauthorized request" });
-      throw "Invalid user ID";
     } else {
       next();
     }
