1a00910680440dd8dcb1ae1d880ff47c1a12377f
ewanouchka <ewanouchka@gmail.com>
2021-11-04 08:37:32 +0100

    edit back add like function

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -6,8 +6,9 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-
     imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+    usersLiked: [],
+    usersDisliked: [],
   });
   sauce
     .save()
@@ -27,7 +28,7 @@ exports.modifySauce = (req, res, next) => {
             }
           : { ...req.body };
         Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Objet modifié !" }))
+          .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
           .catch((error) => res.status(400).json({ error }));
       });
     })
@@ -40,7 +41,7 @@ exports.deleteSauce = (req, res, next) => {
       const filename = sauce.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
           .catch((error) => res.status(400).json({ error }));
       });
     })
@@ -56,5 +57,72 @@ exports.getOneSauce = (req, res, next) => {
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.rateOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const ratedObject = req.body; // {userId, like}
+
+      // si le frontend renvoie 1 (on like une sauce)
+      if (ratedObject.like === 1) {
+        sauce.likes = parseInt(sauce.likes) + 1;
+        sauce.usersLiked.push(ratedObject.userId);
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            likes: sauce.likes,
+            usersLiked: sauce.usersLiked,
+            dislikes: sauce.dislikes,
+            usersDisliked: sauce.usersDisliked,
+            _id: req.params.id,
+          }
+        )
+          .then(() => res.status(200).json({ message: "Vous aimez cette sauce !" }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+
+      // si le frontend renvoie -1 (on dislike une sauce)
+      if (ratedObject.like === -1) {
+        sauce.dislikes = parseInt(sauce.dislikes) + 1;
+        sauce.usersDisliked.push(ratedObject.userId);
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { dislikes: sauce.dislikes, usersDisliked: sauce.usersDisliked, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Vous n'aimez pas cette sauce !" }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+
+      // si le frontend renvoie 0 (le même userID clique à nouveau sur like ou sur dislike)
+      if (ratedObject.like === 0) {
+        if (sauce.usersLiked.indexOf(ratedObject.userId) !== -1) {
+          sauce.likes = parseInt(sauce.likes) - 1;
+          sauce.usersLiked.splice(sauce.usersLiked.indexOf(ratedObject.userId), 1);
+        }
+
+        if (sauce.usersDisliked.indexOf(ratedObject.userId) !== -1) {
+          sauce.dislikes = parseInt(sauce.dislikes) - 1;
+          sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(ratedObject.userId), 1);
+        }
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            likes: sauce.likes,
+            usersLiked: sauce.usersLiked,
+            dislikes: sauce.dislikes,
+            usersDisliked: sauce.usersDisliked,
+            _id: req.params.id,
+          }
+        )
+          .then(() => res.status(200).json({ message: "Votre vote a bien été annulé !" }))
+          .catch((error) => res.status(400).json({ error }));
+      }
+    })
+
     .catch((error) => res.status(400).json({ error }));
 };
